f34c9e0812f5e4ae55757719d6641b805c3c74b3
jonscyr <jons.cyriac@original4sure.com>
2024-07-08 14:33:43 +0530

    add debug logs

--- dist/index.js
+++ dist/index.js
@@ -16,4 +16,17 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./keeper"), exports);
 __exportStar(require("./pool"), exports);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztBQUFBLDJDQUF5QjtBQUN6Qix5Q0FBc0IifQ==
+const debug_1 = require("debug");
+const keeper_1 = require("./keeper");
+const log = (0, debug_1.debug)('keeper');
+setInterval(() => {
+    const entries = Object.entries(keeper_1.availablePools);
+    if (!entries.length) {
+        log('Pool list empty');
+        return;
+    }
+    entries.forEach(([uri, pool]) => {
+        log(JSON.stringify({ uri, info: pool.getInfo() }));
+    });
+}, Number(process.env.LOG_KEEPER_INTERVAL) || 10000);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7OztBQUFBLDJDQUF5QjtBQUN6Qix5Q0FBc0I7QUFDdEIsaUNBQXdDO0FBQ3hDLHFDQUEwQztBQUMxQyxNQUFNLEdBQUcsR0FBRyxJQUFBLGFBQU0sRUFBQyxRQUFRLENBQUMsQ0FBQTtBQUU1QixXQUFXLENBQUMsR0FBRyxFQUFFO0lBQ2IsTUFBTSxPQUFPLEdBQUcsTUFBTSxDQUFDLE9BQU8sQ0FBQyx1QkFBYyxDQUFDLENBQUE7SUFDOUMsSUFBSSxDQUFDLE9BQU8sQ0FBQyxNQUFNLEVBQUU7UUFDbkIsR0FBRyxDQUFDLGlCQUFpQixDQUFDLENBQUE7UUFDdEIsT0FBTTtLQUNQO0lBQ0QsT0FBTyxDQUFDLE9BQU8sQ0FBQyxDQUFDLENBQUMsR0FBRyxFQUFFLElBQUksQ0FBQyxFQUFFLEVBQUU7UUFDOUIsR0FBRyxDQUFDLElBQUksQ0FBQyxTQUFTLENBQUMsRUFBRSxHQUFHLEVBQUUsSUFBSSxFQUFFLElBQUksQ0FBQyxPQUFPLEVBQUUsRUFBRSxDQUFDLENBQUMsQ0FBQTtJQUNwRCxDQUFDLENBQUMsQ0FBQTtBQUNKLENBQUMsRUFBRSxNQUFNLENBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxtQkFBbUIsQ0FBQyxJQUFJLEtBQUssQ0FBQyxDQUFBIn0=
